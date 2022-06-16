070a4e361d8f1c51f9cf4474c319b4c32fc663ac
anson joseph <ansonkj333@gmail.com>
2022-06-16 13:56:39 +0530

    message7

--- listworks/oddlist.py
+++ listworks/oddlist.py
@@ -1,10 +1,16 @@
 # print odd list from list
 
+# list=[10,11,12,13,14,15,16,17,18,19,20]
+# oddlist=[]
+# for num in list:
+#     if num%2!=0:
+#         oddlist.append(num)
+# print(oddlist)
+# oddlist.sort(reverse=True)    #  sorts list in descending order
+# print(oddlist)
+
+#      OR
+
 list=[10,11,12,13,14,15,16,17,18,19,20]
-oddlist=[]
-for num in list:
-    if num%2!=0:
-        oddlist.append(num)
-print(oddlist)
-oddlist.sort(reverse=True)    #  sorts list in descending order
+oddlist=[num for num in list if num%2!=0 ]
 print(oddlist)
