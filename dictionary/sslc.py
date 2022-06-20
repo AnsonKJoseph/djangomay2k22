a6f4deca9bdbab70c74db01b9fbd65b8eb7a5156
anson joseph <ansonkj333@gmail.com>
2022-06-20 14:13:46 +0530

    m

--- dictionary/sslc.py
+++ dictionary/sslc.py
@@ -1,15 +1,38 @@
-results={
-    {"""tvm""":98, "A+":45000},
-    {"ktm":95, "A+":44000},
-    {"apy":97, "A+":47000},
-    {"idk":90, "A+":38000},
-    {"ekm":99, "A+":56000},
-    {"pta":99, "A+":58000},
-    {"tsr":98, "A+":57000},
-    {"kzd":99, "A+":58000},
-    {"pkd":95, "A+":50000},
-    {"mpm":90, "A+":45000},
+results = [
+    {"district": "tvm", "win": 98, "A+": 45000},
+    {"district": "ktm", "win": 95, "A+": 44000},
+    {"district": "apy", "win": 97, "A+": 47000},
+    {"district": "idk", "win": 90, "A+": 38000},
+    {"district": "ekm", "win": 99, "A+": 56000},
+    {"district": "pta", "win": 99, "A+": 58000},
+    {"district": "tsr", "win": 98, "A+": 57000},
+    {"district": "kzd", "win": 99, "A+": 58000},
+    {"district": "pkd", "win": 95, "A+": 50000},
+    {"district": "mpm", "win": 90, "A+": 45000},
 
-}
+]
 
-print(sorted(results,key=lambda i:i[0]))
+# sort result based on win order by desc
+
+print(sorted(results,key=lambda res:res["win"],reverse=True))
+
+# print dist with min win %
+
+print(min(results,key=lambda res:res["win"]))
+
+# sort results based on A+ desc
+
+print(sorted(results,key=lambda res:res["A+"],reverse=True))
+
+# print dist with low count A+
+
+print(min(results,key=lambda res:res["A+"]))
+
+# print total number of students who got full A+
+
+aplus=[res["A+"]for res in results ]                              # list comprehension
+print(sum(aplus))
+
+
+
+
